f082dc5eb8d726012d479d1cba3ec594ded078fa
omargbreil <omargbreil1@gmail.com>
2023-01-06 17:52:52 +0200

    link

--- DB/connection.js
+++ DB/connection.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connection = async () =>
 {
-    return await mongoose.connect(process.env.dbUri)
+    return await mongoose.connect("mongodb+srv://anas:anasanas@cluster0.su1hiba.mongodb.net/register")
     .then(()=>console.log("database connected"))
     .catch(err=>console.log(`${err} database connection error`))
 } 
