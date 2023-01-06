6cd16f1bf068c5c8dbc8f5c9ae6ab1ded3a09f4a
omargbreil <omargbreil1@gmail.com>
2023-01-06 15:47:51 +0200

    deploy3

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
-app.use(`${process.env.baseUrl}/user` , userRouter);
+app.use(`/user` , userRouter);
 connection();
 
 let port = process.env.port
