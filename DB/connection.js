3a18d0d0c39d0563c369d94db057b9139a662af8
omargbreil <omargbreil1@gmail.com>
2023-01-06 17:16:47 +0200

    connection

--- DB/connection.js
+++ DB/connection.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connection = async () =>
 {
-    return await mongoose.connect(process.env.dbUriOnline)
+    return await mongoose.connect(`${process.env.dbUriOnline}`)
     .then(()=>console.log("database connected"))
     .catch(err=>console.log(`${err} database connection error`))
 } 
