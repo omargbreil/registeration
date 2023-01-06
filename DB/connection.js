d7079e2249142250b31c9a903b070455ce044bde
omargbreil <omargbreil1@gmail.com>
2023-01-06 18:41:35 +0200

    ww

--- DB/connection.js
+++ DB/connection.js
@@ -1,9 +1,8 @@
 import mongoose from "mongoose";
-import * as dotenv from "../config/.env"
 
 export const connection = async () =>
 {
-    return await mongoose.connect(`${dotenv.dbUriOnline}`)
+    return await mongoose.connect("mongodb+srv://anas:anasanas@cluster0.su1hiba.mongodb.net/register")
     .then(()=>console.log("database connected"))
     .catch(err=>console.log(`${err} database connection error`))
 } 
